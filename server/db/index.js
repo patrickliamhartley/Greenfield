2bb4f360e8585a4c357fce0bc1edde73a45a6abd
Patrick Hartley <patrickliamhartley@Patricks-MacBook-Pro.local>
2016-09-09 14:40:30 -0700

    mongo fix

--- server/db/index.js
+++ server/db/index.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
-var mongoUri = 'mongodb://patrickliamhartley:muFFin1.@ds021016.mlab.com:21016/greenfield';
+var mongoUri = 'mongodb://localhost/greenfield';
 
 // Connect Mongoose to our local MongoDB via URI specified above and export it below
 var db = mongoose.connect(mongoUri);
