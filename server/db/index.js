1caf1c741926012dc3f4e128d5e6900c8e05206a
Patrick Hartley <patrickliamhartley@Patricks-MacBook-Pro.local>
2016-09-09 14:27:49 -0700

    Changes mongodb for deployment

--- server/db/index.js
+++ server/db/index.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
-var mongoUri = 'mongodb://localhost/greenfield';
+var mongoUri = 'mongodb://heroku_p5trc288:hjv2q2f5kvp5drpdc3p7ccu9hi@ds029106.mlab.com:29106/heroku_p5trc288';
 
 // Connect Mongoose to our local MongoDB via URI specified above and export it below
 var db = mongoose.connect(mongoUri);
